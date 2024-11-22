fcdd6d356451f152d9d9ef3f9a2d2bf2ecd75c85
arzan <arzanchivilakar1902@gmail.com>
2024-11-22 14:16:17 +0530

    first commit

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000');
 });
